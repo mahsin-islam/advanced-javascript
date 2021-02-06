36851568711c632118ea85df0c8985322adb015b
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-02-06 23:57:33 +0600

    JS true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy List:  যেইগুলো সত্য হবে না সেই ধরনের কন্ডিশন
+// false
+// 0 holo negative mean kore
+// ""
+// undefined
+// null
+// NaN not a number
+
+// Truthy List: যেইগুলো সত্য হবে সেই ধরনের কন্ডিশন
+// '0', ' ', [], {}, 
+// koteshoner moddhe ja ja porbe sobi string sudhu empty string tai holo falsy baki sobi truthy
+
+let name = null;
+// let age = 0;
+// if(age || age == 0){
+console.log(name);
+if(name) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
