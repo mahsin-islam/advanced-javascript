48c78250d18a596dadc868056117c4fd730c74d2
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-02-07 00:19:34 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,31 @@
+// undefined mane ami kichu defined kori nai ba diber kotha chilo ami vule gachi
+// undefined 7-8 dhorner pao jate pare 
+
+// let pakhi; // zodi value assign na kori tahole undefined dakhabe varibale r man 1st way
+// console.log(pakhi); 
+// function add(num1, num2){ // kono akta function explecilty jodi kono kichu return na kori tahole sa undefined dakhabe abr return keyword hoitaba likhchi but ki return korbo ba koto return korbo saita likhi nai sai khatreo sa undefined dakhabe 3rd eason holo jodi kono function 2ta parameter thakle akta parameter value na dile tokhon akta value hoitoba dakhbe 2nd value ta undefined return korbe 
+// tarmnae kono akta parameter ka jodi pass na kori tahole by default undefined hoia jai
+//     console.log(num1 + num2);
+// }
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+const result = add(13,82);
+console.log(result);
+
+// dhori amr akta object ache sai object r jodi kono parameter na thake but show kor r somoi unkonown parameter fetch korte chaile undefined dakhabe
+const premik = {name:"smart dude", phone: 458421};
+console.log(premik.gf);
+
+let fun = undefined; // undefined akta reserved word chailai iccha kore aita kora uchit na  aita confusion toiri kore
+console.log(fun);
+
+// null vs undefined common akta interview question prai interview ta ask kore ai 2ter moddhe differnece ki
+// undefined onek vabe pate pari
+// like
+let ages = [2,5,9];
+console.log(ages[11]); // aikhne 11 tomo elemnts nai to sai jonne undefined dakhabe
+
+// null jinista explecitly set korte hobe ja oikhane nai ba chilo khali hoia gache sai khatere ami null set korbo
+
